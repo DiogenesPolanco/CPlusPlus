48cc932193824784f1d03a0f3ce19596ca476196
reymarte <rendey28@hotmail.com>
2017-03-23 21:07:37 -0400

    tarea 06 done

--- 06_tarea_arreglos.cpp
+++ 06_tarea_arreglos.cpp
@@ -7,15 +7,22 @@ int main()
 {
 
     //Declaración del arreglo de 10 elementos de tipo int
-    int a[10];
+    int a[10]={5,10,15,20,25,30,35,40,45,};
+    int n, result;
+    
 
     //Inicialización del arreglo, aqui tienes que hacer todo el trabajo
     
-    //Se debe tomar cada una de las posiciones y asignarles un valor
- 
-	//Valor inicial para la posición i-ésima del arreglo
+    for (n=0;n<10; n++){
+        result = a[n];
+    }
 
+
+    
+    //Se debe tomar cada una de las posiciones y asignarles un valor    
+ 	//Valor inicial para la posición i-ésima del arreglo    
     //Mostrar el valor de la información almacenada en el arreglo
+    cout << "Resultado= " << result << endl;
 
     return 0;
 }
